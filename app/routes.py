e17890a8abd03cfad90c9facb7893489180e00f4
melaniesarah <melaniesarah@gmail.com>
2020-09-16 16:54:30 -0500

    Home route now utilizes index template

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,8 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
+@app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Melanie'}
+    return render_template('index.html', title='Home', user=user)
