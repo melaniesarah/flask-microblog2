4d16c9334150045939bcba2dccd1d67553bc7929
melaniesarah <melaniesarah@gmail.com>
2020-09-23 14:49:48 -0500

    Setup User model, db and ORM layer

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-from app import routes
+from app import routes, models
 '''
 The bottom import is a workaround to circular imports, a common problem with Flask applications.
 The routes module needs to import the app variable defined in this script, so putting one of the
--- config.py
+++ config.py
@@ -1,4 +1,13 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    '''
+    The SQLALCHEMY_TRACK_MODIFICATIONS configuration option is set to False to disable the feature of Flask-SQLAlchemy 
+    that signals the app every time a change is about to be made in the database.
+    '''
