9782ca99662a5b2983ce59f7d36f2e80da378318
melaniesarah <melaniesarah@gmail.com>
2020-09-25 18:14:27 -0500

    Add user profile route, link,  and page

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, login_required
 from app import app
 from app.forms import LoginForm
 
@@ -25,3 +25,13 @@ def login():
         flash('Login requested for user {}, rembember_me={}'.format(form.username.data, form.remember_me.data))
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
