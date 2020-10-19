dfadc8de0c6373636d3b846ac821ed42231b3e4e
melaniesarah <melaniesarah@gmail.com>
2020-10-19 16:23:17 -0500

    Implement followers and following

--- config.py
+++ config.py
@@ -6,13 +6,13 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    '''
-    The SQLALCHEMY_TRACK_MODIFICATIONS configuration option is set to False to disable the feature of Flask-SQLAlchemy 
-    that signals the app every time a change is about to be made in the database.
-    '''
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['melaniehahn.devloper@gmail.com']
+    ADMINS = ['melaniesarah@gmail.com']
+    '''
+    The SQLALCHEMY_TRACK_MODIFICATIONS configuration option is set to False to disable the feature of Flask-SQLAlchemy 
+    that signals the app every time a change is about to be made in the database.
+    '''
