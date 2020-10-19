6773276dbd31ceef003836aa6004bf15d99ee3b5
melaniesarah <melaniesarah@gmail.com>
2020-10-19 18:58:27 -0500

    Users see sorted followed and own posts

--- app/models.py
+++ app/models.py
@@ -51,10 +51,12 @@ class User(UserMixin, db.Model):
         return self.followed.filter(followers.c.followed_id == user.id).count() > 0
 
     def followed_posts(self):
-        return Post.query.join(
+        followed = Post.query.join(
             followers, (followers.c.followed_id == Post.user_id)).filter(
-                followers.c.follower_id == self.id).order_by(
-                    Post.timestamp.desc())
+                followers.c.follower_id == self.id)
+            own = Post.query.filter_by(user_id=self.id)
+                .order_by(
+            return followed.union(own).order_by(Post.timestamp.desc())
             
         
 
