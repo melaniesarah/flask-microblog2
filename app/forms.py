8b37992cb6f30b2cf3ad6feb10acd82facbc2a21
melaniesarah <melaniesarah@gmail.com>
2020-11-05 16:49:39 -0600

    Integrate follow/unfollow capabilities

--- app/forms.py
+++ app/forms.py
@@ -35,3 +35,6 @@ class EditProfileForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
     submit = SubmitField('Submit')
+
+class EmptyForm(FlaskForm):
+    submit = SubmitField('Submit')
