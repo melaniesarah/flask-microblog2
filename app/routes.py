00f2346fd45b7eed0e53a7a081491b11284adeb9
melaniesarah <melaniesarah@gmail.com>
2021-01-17 10:23:22 -0600

    I18n and L10n for moment.js library

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from datetime import datetime
-from flask import render_template, flash, redirect, url_for, request
+from flask import render_template, flash, redirect, url_for, request, g
 from flask_login import login_user, logout_user, current_user, login_required
-from flask_babel import _
+from flask_babel import _, get_locale
 from werkzeug.urls import url_parse
 from app import app, db
 from app.forms import LoginForm, RegistrationForm, EditProfileForm, \
@@ -14,6 +14,7 @@ def before_request():
     if current_user.is_authenticated:
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
+    g.locale = str(get_locale())
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
